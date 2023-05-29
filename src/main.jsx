b81dfc3b9e07fa0f8c3ec39ccb2a95039d8f4be2
Tegar Risqy Yulian Santoso <tegarrizky237@gmail.com>
2023-05-29 18:27:46 +0700

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -13,4 +12,6 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+
 
